d1555b916f46d8ec01f5ed40eebc31d06049db18
Tatiana <zepolitat@mateubuntuSSD.(none)>
2013-03-15 08:17:46 -0500

    New test images added.

--- msc/cv2013/hw1/octave/z0projective_t.m
+++ msc/cv2013/hw1/octave/z0projective_t.m
@@ -14,9 +14,8 @@ clear; clc; close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Load transformed image >>projected<<
-%[ztransf, imgtransf, W, H] = zimread('../imgs/building.png');
 [ztransf, imgtransf, W, H] = zimread('../imgs/tiles.png');
-%[ztransf, imgtransf, W, H] = zimread('../imgs/test_1.jpg');
+%[ztransf, imgtransf, W, H] = zimread('../imgs/test_2.jpg');
 
 imshow(imgtransf);
 hold on;
@@ -25,12 +24,11 @@ hold on;
 [x,y] = ginput(4);
 scatter(x,y,5,'r');
 
-%TODO: Ask the teacher how to get these values o.O
+%TODO: Ask how to get these values o.O
 %xp=[50;130;130;50];
 %yp=[50;50;130;130];
 xp=[0;W;W;0];
 yp=[0;0;H;H];
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Find transformation matrix from 2 sets of 4 points in the
@@ -38,18 +36,19 @@ yp=[0;0;H;H];
 Hmat = zfindH(x,y,xp,yp);
 Hinv = pinv(Hmat);
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Finds the corresponding original no transformed points (x,y) 
-ix1dorig = zfindX(Hinv,W,H);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Create new vector image from transformed indexes and transformed rgb channels
-zorig = zmaptovec(ix1dorig, ztransf, W*H);
-%Transform vector to matrix image
-imgorig = zimasmat(zorig, W, H);
+imgtransf2 = ones(H,W,3);
+for i=1:size(imgtransf2)(1)
+    for j=1:size(imgtransf2)(2)
+	tx = Hinv*[i j 1]';
+	tx = int32(tx./tx(3));
+	if(tx(1)>0 && tx(1)< size(imgtransf2)(1) &&
+	   tx(2)>0 && tx(2)< size(imgtransf2)(2))
+		imgtransf2(tx(1),tx(2),:)=imgtransf(i,j,:);
+	end
+    end
+end
 
 figure(2);
-imshow(imgorig);
-
+imshow(uint8(imgtransf2));
 
--- msc/cv2013/hw1/octave/zmaptovec.m
+++ msc/cv2013/hw1/octave/zmaptovec.m
@@ -1,3 +1,15 @@
+#######################################################
+## usage: zorig = zmaptovec(ix1dorig, ztransf, lsup)
+##
+## Uses the index vector with the equivalent positions
+## of the original image to map the corresponding
+## rgb values into the zorig vector
+
+## Author: Tatiana L. G. <tatiana@sirius.utp.edu.co>
+## Created: 
+## Version: 0.0.1
+#######################################################
+ 
 function zorig = zmaptovec(ix1dorig, ztransf, lsup)
 
     %Create new image in 1dvec form
@@ -7,7 +19,6 @@ function zorig = zmaptovec(ix1dorig, ztransf, lsup)
     validpos = find(ix1dorig>0 & ix1dorig<=lsup);
 
     %Assign RGB channel of the transformed image (x',y') into the equivalent original image (x,y)
-    %zorig(ix1dorig>0,:) = ztransf(find(ix1dorig>0),:);
     zorig(validpos,:) = ztransf(ix1dorig(validpos),:);
 
 end
